1a4ae0d2415b92a949cfc42d2ec3ab4a023b915e
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-01-23 14:28:29 -0300

    subdomain em comentários

--- app/controllers/laboratorios_controller.rb
+++ app/controllers/laboratorios_controller.rb
@@ -1,7 +1,7 @@
 class LaboratoriosController < ApplicationController
   def show
     unless @lab
-      @subdomain = request.subdomain
+      #@subdomain = request.subdomain
       render :inexistente
     end
   end
