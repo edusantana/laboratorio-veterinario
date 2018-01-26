db60344fde4979ef01704742a8468dedc39a6538
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-01-26 09:31:03 -0300

    teste envio de e-mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "laboratorio-veterinario_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_SERVER'],
+    port:                 587,
+#    domain:               'example.com',
+    user_name:            ENV['AWS_SMTP_USERNAME'],
+    password:             ENV['AWS_SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
