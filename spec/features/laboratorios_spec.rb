647c91265db266a7ac13b614fbfdbcad823868e0
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-01-26 09:30:48 -0300

    refactory

--- spec/features/laboratorios_spec.rb
+++ spec/features/laboratorios_spec.rb
@@ -76,51 +76,6 @@ RSpec.feature "Laboratorios", type: :feature do
     end
   end
 
-  def dado_um_laboratorio
-    @lab = create(:laboratorio)
-  end
-  def dado_um_laboratorio_inexistente
-    @lab = build(:laboratorio)
-  end
-  
-  def dado_um_laboratorio_com_funcionarios
-    @lab = create(:laboratorio_com_funcionarios)
-    @tecnico = User.with_role(:tecnico, @lab).take
-    @secretario = User.with_role(:secretario, @lab).take
-  end
-
-  def dado_um_laboratorio_com_uma_unidade
-    dado_um_laboratorio
-    @unidade = create(:unidade, laboratorio: @lab)
-    @lab.unidades << @unidade
-  end
-
-  def dado_um_laboratorio_com_alguns_tipos_de_exames_cadastrados
-    dado_um_laboratorio
-    @tipos = []
-    @tipos << create(:exame_tipo, laboratorio: @lab)
-  end
-
-  def e_um_usuario_logado
-    usando_labdomain(@lab)
-    @user = create(:user)
-    login(@user)
-  end
-
-  def e_um_tecnico_logado
-    usando_labdomain(@lab)
-    login(@tecnico)
-  end
-
-  def e_um_secretario_logado
-    usando_labdomain(@lab)
-    login(@secretario)
-  end
-
-  def quando_acessar_subdominio_do_laboratorio
-    usando_labdomain(@lab)
-    visit root_path
-  end
 
   def entao_pagina_inicial_do_laboratorio_eh_exibida
     expect(page).to have_content(@lab.nome)
