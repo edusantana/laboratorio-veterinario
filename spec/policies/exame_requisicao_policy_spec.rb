8addb187ba98db913a45ba63804caed670a718e7
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-02-13 15:57:30 -0300

    Atualizações de permissões

--- spec/policies/exame_requisicao_policy_spec.rb
+++ spec/policies/exame_requisicao_policy_spec.rb
@@ -8,22 +8,19 @@ describe ExameRequisicaoPolicy do
   let(:lab){create(:laboratorio_com_funcionarios)}
   let(:requisicao){create(:requisicao, laboratorio: lab)}
   let(:cliente){create(:user)}
-  let(:secretario){User.with_role(:secretario, lab.organizacao).take}
-  let(:tecnico){User.with_role(:tecnico, lab.organizacao).take}
-  let(:dono){User.with_role(:dono, lab.organizacao).take}
   let(:requisitante){requisicao.requisitante}
 
   context 'sendo um cliente da organização do laboratório' do
     let(:user){cliente}
     it { is_expected.to permit_action(:new) }
     it { is_expected.to permit_action(:create) }
-    it { is_expected.not_to permit_action(:update) }
+    context "tentando atualizar requisição de outro cliente" do
+      it { is_expected.not_to permit_action(:update) }
+    end
   end
 
   context 'sendo o requisitante da requisição' do
     let(:user){requisitante}
-    it { is_expected.to permit_action(:new) }
-    it { is_expected.to permit_action(:create) }
     it { is_expected.to permit_action(:update) }
 
     context "requisição já foi recebida" do
